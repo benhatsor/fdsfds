83ec5fd4d6515ee7d55791c3a5b47a891a5670a3
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2023-01-16 20:19:52 +0200

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,11 @@
-console.log('hello')
+let copy = async (text) => {
+  
+  await navigator.clipboard.writeText(text);
+  
+}
 
+document.onclick = (e) => {
+    if (e.target.classList.contains('materialStyle')) { 
+        copy(e.target.textContent);
+    }
+}
